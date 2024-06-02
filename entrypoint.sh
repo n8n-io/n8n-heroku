419fe20069c1e89c94ccf5519399e678950769f3
Prakash Maheshwaran <73785492+Prakashmaheshwaran@users.noreply.github.com>
2024-06-02 23:52:53 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,20 @@
 #!/bin/sh
 
-# Check if port variable is set or go with default
-if [ -z ${PORT+x} ]; then 
-  echo "PORT variable not defined, leaving N8N to default port."
-else 
-  export N8N_PORT="$PORT"
-  echo "N8N will start on '$PORT'"
-fi
+# check if port variable is set or go with default
+if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT="$PORT"; echo "N8N will start on '$PORT'"; fi
 
-# Use DATABASE_URL_WITH_SSL instead of DATABASE_URL
-DATABASE_URL=$DATABASE_URL_WITH_SSL
-
-# Function to parse URL
+# regex function
 parse_url() {
-  eval $(echo "$1" | sed -e "s#^\(\(.*\)://\)\?\(\([^:@]*\)\(:\(.*\)\)\?@\)\?\([^/?]*\)\(/[^?]*\)\?\?\(.*\)#${PREFIX:-URL_}SCHEME='\2' ${PREFIX:-URL_}USER='\4' ${PREFIX:-URL_}PASSWORD='\6' ${PREFIX:-URL_}HOSTPORT='\7' ${PREFIX:-URL_}DATABASE='\8' ${PREFIX:-URL_}QUERY='\9'#")
+  eval $(echo "$1" | sed -e "s#^\(\(.*\)://\)\?\(\([^:@]*\)\(:\(.*\)\)\?@\)\?\([^/?]*\)\(/\(.*\)\)\?#${PREFIX:-URL_}SCHEME='\2' ${PREFIX:-URL_}USER='\4' ${PREFIX:-URL_}PASSWORD='\6' ${PREFIX:-URL_}HOSTPORT='\7' ${PREFIX:-URL_}DATABASE='\9'#")
 }
 
-# Parse the DATABASE_URL_WITH_SSL
+# prefix variables to avoid conflicts and run parse url function on arg url
 PREFIX="N8N_DB_" parse_url "$DATABASE_URL"
 echo "$N8N_DB_SCHEME://$N8N_DB_USER:$N8N_DB_PASSWORD@$N8N_DB_HOSTPORT/$N8N_DB_DATABASE"
-
-# Separate host and port
+# Separate host and port    
 N8N_DB_HOST="$(echo $N8N_DB_HOSTPORT | sed -e 's,:.*,,g')"
 N8N_DB_PORT="$(echo $N8N_DB_HOSTPORT | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
 
-# Set environment variables
 export DB_TYPE=postgresdb
 export DB_POSTGRESDB_HOST=$N8N_DB_HOST
 export DB_POSTGRESDB_PORT=$N8N_DB_PORT
@@ -33,5 +23,5 @@ export DB_POSTGRESDB_USER=$N8N_DB_USER
 export DB_POSTGRESDB_PASSWORD=$N8N_DB_PASSWORD
 export DB_POSTGRESDB_SSL=true  # Ensure SSL is enabled
 
-# Kickstart nodemation
+# kickstart nodemation
 n8n
